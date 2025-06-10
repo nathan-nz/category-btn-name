3956cb051d1b110d9889ef6912e3dcfe95f5cfa3
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2025-06-10 14:03:41 +1200

    Update initialize-btn-names.js

--- javascripts/discourse/api-initializers/initialize-btn-names.js
+++ javascripts/discourse/api-initializers/initialize-btn-names.js
@@ -3,13 +3,15 @@ import discourseComputed from "discourse-common/utils/decorators";
 
 export default apiInitializer("0.8", (api) => {
   const CATEGORY_LABELS = {};
+  const defaultLabel = settings.default_button_label || "New Topic";
 
   try {
     const buttonLabels = settings.category_button_labels.split("\n");
     buttonLabels.forEach((entry) => {
-      const [categoryId, label] = entry.split(":");
-      if (categoryId && label) {
-        CATEGORY_LABELS[parseInt(categoryId.trim(), 10)] = label.trim();
+      const [key, label] = entry.split(":");
+      const categoryId = parseInt(key.trim(), 10);
+      if (!isNaN(categoryId) && label) {
+        CATEGORY_LABELS[categoryId] = label.trim();
       }
     });
   } catch (e) {
@@ -18,11 +20,11 @@ export default apiInitializer("0.8", (api) => {
 
   api.modifyClass("component:d-navigation", {
     pluginId: "category-btn-name",
-    
+
     @discourseComputed("hasDraft", "category")
     createTopicLabel(hasDraft, category) {
-      if (!hasDraft && category && CATEGORY_LABELS[category.id]) {
-        return CATEGORY_LABELS[category.id];
+      if (!hasDraft && category) {
+        return CATEGORY_LABELS[category.id] || defaultLabel;
       } else {
         return this._super(hasDraft, category);
       }
