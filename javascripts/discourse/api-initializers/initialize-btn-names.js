33d62ae69c2d6442046734998dc46fb55da28a68
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2025-06-10 14:37:42 +1200

    Update initialize-btn-names.js

--- javascripts/discourse/api-initializers/initialize-btn-names.js
+++ javascripts/discourse/api-initializers/initialize-btn-names.js
@@ -6,18 +6,17 @@ const TRANSLATION_KEYS = {
   // Add additional categories and their respective translation keys here
 };
 
+
 export default apiInitializer("0.8", (api) => {
   api.modifyClass("component:d-navigation", {
-    pluginId: "category-btn-name",
+     pluginId: "category-btn-name",
     
     @discourseComputed("hasDraft", "category")
     createTopicLabel(hasDraft, category) {
       if (!hasDraft && category && TRANSLATION_KEYS[category.name]) {
-        // Use the specified translation key for this category
-        return I18n.t(`js.${TRANSLATION_KEYS[category.name]}`);
+        return themePrefix(TRANSLATION_KEYS[category.name]);
       } else {
-        // Use the default translation key for "New Topic"
-        return I18n.t('js.composer.create_topic.title');
+        return this._super(hasDraft);
       }
     },
   });
