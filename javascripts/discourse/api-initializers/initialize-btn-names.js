1dc9e30a0e6c01ecf7011bda2111a861efea0064
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2025-06-10 13:04:14 +1200

    Update initialize-btn-names.js

--- javascripts/discourse/api-initializers/initialize-btn-names.js
+++ javascripts/discourse/api-initializers/initialize-btn-names.js
@@ -1,18 +1,27 @@
 import { apiInitializer } from "discourse/lib/api";
 import discourseComputed from "discourse-common/utils/decorators";
 
-const TRANSLATION_KEYS = {
-  "Events": "new_event",
-};
-
 export default apiInitializer("0.8", (api) => {
+  const CATEGORY_LABELS = {};
+  
+  try {
+    settings.category_button_labels.forEach((entry) => {
+      const { category, label } = entry;
+      if (category && label) {
+        CATEGORY_LABELS[category.trim()] = label.trim();
+      }
+    });
+  } catch (e) {
+    console.error("Error parsing category_button_labels setting:", e);
+  }
+
   api.modifyClass("component:d-navigation", {
-     pluginId: "category-btn-name",
+    pluginId: "category-btn-name",
     
     @discourseComputed("hasDraft", "category")
     createTopicLabel(hasDraft, category) {
-      if (!hasDraft && category && TRANSLATION_KEYS[category.name]) {
-        return themePrefix(TRANSLATION_KEYS[category.name]);
+      if (!hasDraft && category && CATEGORY_LABELS[category.name]) {
+        return CATEGORY_LABELS[category.name];
       } else {
         return this._super(hasDraft);
       }
